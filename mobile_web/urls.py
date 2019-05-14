e8e4245d4c3dd3d107c51cd1822c875c230ad98d
partum <partum@ubuntu.ubuntu-domain>
2019-05-14 07:19:28 -0400

    mobile common views and urls

--- mobile_web/urls.py
+++ mobile_web/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views.common_views import MobileHomePageView
+from .views.common_views import (MobileHomePageView,MobileRegisterView, MobileLoginView,
+                                 MobileUserInfo)
+
 
 urlpatterns = [
     path('', MobileHomePageView.as_view(), name='home'),
+    path('register/', MobileRegisterView.as_view(), name='register'),
+    path('login/', MobileLoginView.as_view(), name='login'),
+    path('user/<int:pk>/', MobileUserInfo.as_view(), name='user_info'),
+
 ]
--- mobile_web/views/common_views.py
+++ mobile_web/views/common_views.py
@@ -1,14 +1,20 @@
 from django.views.generic import TemplateView
+from common.views import RegisterView, LoginView,HomeView, UpdateProfile
 
-class MobileRegisterView(TemplateView):
-    pass
 
-class MobileLoginView(TemplateView):
-    pass
+class MobileRegisterView(RegisterView):
+    template_name = 'mobile_web/register.html'
+
+
+class MobileLoginView(LoginView):
+    template_name = 'mobile_web/login.html'
 
 class MobileLogoutView(TemplateView):
     pass
 
 
-class MobileHomePageView(TemplateView ):
+class MobileHomePageView(HomeView):
     template_name = 'mobile_web/home.html'
+
+class MobileUserInfo(UpdateProfile):
+    template_name = 'mobile_web/user.html'
