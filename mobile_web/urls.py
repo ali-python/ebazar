0a8c50fdcca27f13dc05edd6aa3137a9e804560c
partum <partum@ubuntu.ubuntu-domain>
2019-05-20 12:57:15 -0400

    merchant mobile views

--- mobile_web/urls.py
+++ mobile_web/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from .views.common_views import (MobileHomePageView,MobileRegisterView, MobileLoginView,
-                                 MobileUserInfo)
+                                 MobileUserInfo, MobileMerchantCreateDailyRecordView,
+                                 MobileMerchantDashboardView, MobileMerchantDailyRecordsView,
+                                 MobileMerchantSalesRecordsView, MobileMerchantUpdateDailyRecordView
+                                 )
 
 
 urlpatterns = [
@@ -9,5 +12,10 @@ urlpatterns = [
     path('register/', MobileRegisterView.as_view(), name='register'),
     path('login/', MobileLoginView.as_view(), name='login'),
     path('user/<int:pk>/', MobileUserInfo.as_view(), name='user_info'),
+    path('merchant/dashboard/view/', MobileMerchantDashboardView.as_view(), name='merchant_dashboard'),
+    path('merchant/create/<int:pk>/daily/record/', MobileMerchantCreateDailyRecordView.as_view(), name='merchant_create_record'),
+    path('merchant/daily/record/view/', MobileMerchantDailyRecordsView.as_view(), name='merchant_daily_record'),
+    path('merchant/sales/record/view/', MobileMerchantSalesRecordsView.as_view(), name='merchant_sales_record'),
+    path('merchant/update/<int:pk>/view/', MobileMerchantUpdateDailyRecordView.as_view(), name='merchant_update_record'),
 
 ]
--- mobile_web/views/common_views.py
+++ mobile_web/views/common_views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView, UpdateView
+from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.shortcuts import render
+from django.contrib.auth import forms as auth_forms
+from django.db import transaction
+from django.contrib.auth import authenticate
+from django.db.models import Sum
+
+from django.contrib.auth import login as auth_login
+from merchant.models import MerchantDailyRecord, MerchantSalesRecords
 from common.views import RegisterView, LoginView,HomeView, UpdateProfile
+from merchant.views import (DashboardView,DailyRecordsView,SalesRecordsView,
+                            CreateDailyRecordView,UpdateDailyRecordView)
 
 
 class MobileRegisterView(RegisterView):
@@ -8,6 +21,29 @@ class MobileRegisterView(RegisterView):
 
 class MobileLoginView(LoginView):
     template_name = 'mobile_web/login.html'
+    form_class = auth_forms.AuthenticationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('mobile:home'))
+
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.get_user()
+        auth_login(self.request, user)
+        return HttpResponseRedirect(reverse('mobile:login'))
+
+    def form_invalid(self, form):
+        return super(LoginView, self).form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        merchant_daily_records = MerchantDailyRecord.objects.all()
+        context.update({
+            'merchant_daily_records': merchant_daily_records
+        })
+        return context
 
 class MobileLogoutView(TemplateView):
     pass
@@ -15,6 +51,68 @@ class MobileLogoutView(TemplateView):
 
 class MobileHomePageView(HomeView):
     template_name = 'mobile_web/home.html'
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            if self.request.user.user_profile:
+                if (
+                        self.request.user.user_profile.type ==
+                        self.request.user.user_profile.USER_TYPE_MERCHANT
+                ):
+                    return HttpResponseRedirect(reverse('mobile:merchant_dashboard'))
+        return super(
+            HomeView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        merchant_daily_records=MerchantDailyRecord.objects.all()
+        context.update({
+            'merchant_daily_records': merchant_daily_records
+        })
+
 
 class MobileUserInfo(UpdateProfile):
     template_name = 'mobile_web/user.html'
+
+#------------------ Merchant Mobile Views -------------------- #
+
+class MobileMerchantDashboardView(DashboardView):
+    template_name ='mobile_web/merchant_dashboard.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('mobile:merchant_daily_record'))
+        return super(
+            DashboardView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        daily_records = MerchantDailyRecord.objects.filter(
+            merchant=self.request.user.user_merchant.merchant
+        ).latest('id')
+        sales = MerchantSalesRecords.objects.filter(
+            merchant_daily_record__merchant=self.request.user.user_merchant.merchant
+        )
+        purchased_price = sales.aggregate(Sum('purchased_price'))
+        purchased_price = purchased_price.get('purchased_price__sum')
+        purchased_quantity = sales.aggregate(Sum('purchased_quantity'))
+        purchased_quantity = purchased_quantity.get('purchased_quantity__sum')
+        remaining_quantity = float(daily_records.item_quantity) - float(purchased_quantity)
+        context.update({
+            'purchased_price': purchased_price,
+            'purchased_quantity': purchased_quantity,
+            'daily_records': daily_records,
+            'remaining_quantity': remaining_quantity
+        })
+        return context
+
+class MobileMerchantDailyRecordsView(DailyRecordsView):
+    template_name ='mobile_web/merchant_daily_records.html'
+
+class MobileMerchantSalesRecordsView(SalesRecordsView):
+    template_name ='mobile_web/merchant_sales_record.html'
+
+class MobileMerchantCreateDailyRecordView(CreateDailyRecordView):
+    template_name ='mobile_web/merchant_create_daily_record.html'
+
+class MobileMerchantUpdateDailyRecordView(UpdateDailyRecordView):
+    template_name ='mobile_web/merchant_update_record.html'
