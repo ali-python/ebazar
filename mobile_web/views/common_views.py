edbddc0bf68ceedb3aef9127c38102b991536a51
partum <partum@ubuntu.ubuntu-domain>
2019-05-22 13:41:59 -0400

    client mobile views and urls

--- mobile_web/views/common_views.py
+++ mobile_web/views/common_views.py
@@ -11,10 +11,13 @@ from django.db.models import Sum
 from django.contrib.auth import login as auth_login
 from merchant.models import MerchantDailyRecord, MerchantSalesRecords
 from merchant.forms import MerchantDailyRecordForm
+from client.forms import OrderItemForm
+from client.models import Order, Invoice
 from common.views import RegisterView, LoginView,HomeView, UpdateProfile
 from merchant.views import (DashboardView,DailyRecordsView,SalesRecordsView,
                             CreateDailyRecordView,UpdateDailyRecordView)
-
+from client.views import (MerchantDailyRecordDetailView,OrderItemView ,ClientInvoice ,InvoiceHistoery)
+from common.models import City
 
 class MobileRegisterView(RegisterView):
     template_name = 'mobile_web/register.html'
@@ -61,14 +64,15 @@ class MobileHomePageView(HomeView):
                 ):
                     return HttpResponseRedirect(reverse('mobile:merchant_dashboard'))
         return super(
-            HomeView, self).dispatch(request, *args, **kwargs)
+            MobileHomePageView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
+        context = super(MobileHomePageView, self).get_context_data(**kwargs)
         merchant_daily_records=MerchantDailyRecord.objects.all()
         context.update({
             'merchant_daily_records': merchant_daily_records
         })
+        return context
 
 
 class MobileUserInfo(UpdateProfile):
@@ -132,3 +136,116 @@ class MobileMerchantUpdateDailyRecordView(UpdateDailyRecordView):
     model = MerchantDailyRecord
     success_url = reverse_lazy('mobile:merchant_daily_record')
 
+#-------------------client mobile views -----------------------------#
+
+class MobileMerchantDailyRecordDetailView(TemplateView):
+    template_name = 'moblie_web/merchant_record_detail.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('mobile:login'))
+        return super(
+            MobileMerchantDailyRecordDetailView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(MobileMerchantDailyRecordDetailView, self).get_context_data(**kwargs)
+        merchant = MerchantDailyRecord.objects.get(id=self.kwargs.get('pk'))
+        city = City.objects.all()
+
+        context.update({
+            'merchant': merchant,
+            'city': city
+        })
+        return context
+
+class MobilerOrderItemView(FormView):
+    form_class = OrderItemForm
+    template_name = 'mobile_web/client_order.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('client:list_records_view'))
+        return super(
+            MobilerOrderItemView, self).dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            obj = form.save(commit=False)
+
+            obj.order = Order.objects.create(
+                customer_name=self.request.POST.get('customer_name'),
+                customer_phone=self.request.POST.get('customer_phone'),
+                alternate_phone=self.request.POST.get('alternate_phone'),
+                user=self.request.user,
+            )
+
+            obj.city = City.objects.get(city_name=self.request.POST.get('city'))
+            obj.city.save()
+            invoice = Invoice.objects.create(
+                order=obj.order,
+                amount=obj.item_price
+            )
+            invoice.save()
+            merchant_sales_record = MerchantSalesRecords.objects.create(
+                merchant_daily_record=obj.merchant_daily_upload,
+                purchased_quantity=obj.item_quantity,
+                purchased_price=obj.item_price
+            )
+            merchant_sales_record.save()
+            self.request.user.user_profile.phone = self.request.POST.get('customer_phone')
+            self.request.user.user_profile.alternate_phone = self.request.POST.get('alternate_phone')
+            self.request.user.user_profile.address = self.request.POST.get('address')
+            self.request.user.user_profile.save()
+            obj.save()
+            return HttpResponseRedirect(reverse('mobile:client_order_invoice', kwargs={
+                'pk': invoice.id
+            }))
+
+    def form_invalid(self, form):
+        return super(MobilerOrderItemView, self).form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(MobilerOrderItemView, self).get_context_data(**kwargs)
+        merchant_record = MerchantDailyRecord.objects.get(id=self.kwargs.get('pk'))
+        context.update({
+            'merchant_record': merchant_record
+        })
+        return context
+
+class MobileClientInvoice(TemplateView):
+    template_name = 'mobile_web/client_invoice.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('mobile:home'))
+        return super(
+            MobileClientInvoice, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(MobileClientInvoice, self).get_context_data(**kwargs)
+        invoice = Invoice.objects.get(id=self.kwargs.get('pk'))
+        order_items = invoice.order.customer_order.all()
+
+        from django.db.models import Sum
+        try:
+            total_quantity = order_items.aggregate(Sum('item_quantity'))
+            total_quantity = total_quantity.get('item_quantity__sum') or 0
+
+            total_price = order_items.aggregate(Sum('item_price'))
+            total_price = total_price.get('item_price__sum') or 0
+        except:
+            total_quantity = 0
+            total_price = 0
+
+        context.update({
+            'invoice': invoice,
+            'order_items': order_items,
+            'total_quantity': total_quantity,
+            'total_price': total_price
+            # 'order':order
+
+        })
+        return context
+
+class MobileInvoiceHistoery(InvoiceHistoery):
+    template_name = 'mobile_web/client_invoice_list.html'
